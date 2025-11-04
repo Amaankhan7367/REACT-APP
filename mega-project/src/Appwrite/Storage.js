893102d244ec4936509ef9eda337afe07ff20f8f
Amaankhan7367 <158289598+Amaankhan7367@users.noreply.github.com>
2025-11-04 13:06:07 +0530

    update mega-project/src/Appwrite/Storage.js

--- mega-project/src/Appwrite/Storage.js
+++ mega-project/src/Appwrite/Storage.js
@@ -1,2 +1,15 @@
 import conf from '../Conf/conf.js';
-import {Id,clinte,Storage,Databases,Query} from 'appwrite';
+import {Id,Clinte,Storage,Databases,Query} from 'appwrite';
+
+class Service{
+  clinte=new Clinte();
+  database
+  bucket;
+  
+this.clinte 
+    .setEndpoint(conf.appwriteUrl)
+    .setProject(conf.appwriteProjectId)
+    this.Database(this.clinte)
+}
+comst service= new Service();
+export default service;
