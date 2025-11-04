a6ff5c479a80019e7447dfd9ab68b39198f1ea89
Amaankhan7367 <158289598+Amaankhan7367@users.noreply.github.com>
2025-11-04 13:16:15 +0530

    update mega-project/src/Appwrite/Storage.js

--- mega-project/src/Appwrite/Storage.js
+++ mega-project/src/Appwrite/Storage.js
@@ -4,12 +4,21 @@ import {Id,Clinte,Storage,Databases,Query} from 'appwrite';
 class Service{
   clinte=new Clinte();
   database
-  bucket;
+  bucket
   
+  constructor(){
 this.clinte 
     .setEndpoint(conf.appwriteUrl)
     .setProject(conf.appwriteProjectId)
     this.database=Databases(this.clinte);
+    this.bucket=Storage(this.clinte);
+  }
+  async createPost({title,slug,content,image,status,userId}){
+    await this.database.createDocument(
+      conf.appwriteDatabaseId;
+      conf.appwriteCollectionId
+      )
+  }
 }
 
 comst service= new Service();
